2780794bd57e845ef86cac54dd9e708a058c396c
sajibkhandev <sajibkhan.mern@gmail.com>
2026-02-17 19:26:14 +0600

    feat: show more product

--- src/layouts/ExploreProduct.jsx
+++ src/layouts/ExploreProduct.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Container from '../components/Container'
 import SubHeading from '../components/SubHeading'
 import Heading from '../components/Heading'
@@ -6,8 +6,14 @@ import Flex from '../components/Flex'
 import ProductOne from "../assets/product1.png";
 import Card from '../components/Card'
 import Button from '../components/Button'
+import ApiData from '../data'
+console.log(ApiData.length);
+
 
 const ExploreProduct = () => {
+    let [show,setShow]=useState(4)
+    
+   
   return (
      <section className='pb-[140px]'>
             <Container>
@@ -15,26 +21,23 @@ const ExploreProduct = () => {
                     <Heading className="pt-6 pb-[60px]" text="Best Selling Products" />
                
                 <Flex className='justify-between flex-wrap gap-y-10'>
-                    <Card image={ProductOne} title="HAVIT HV-G92 Gamepad" saleprice="120" regularprice="160" badge="new"
-                    />
-                    <Card image={ProductOne} title="HAVIT HV-G92 Gamepad" saleprice="120" regularprice="160" badge="new"
-                    />
-                    <Card image={ProductOne} title="HAVIT HV-G92 Gamepad" saleprice="120" regularprice="160" badge="new"
-                    />
-                    <Card image={ProductOne} title="HAVIT HV-G92 Gamepad" saleprice="120" regularprice="160" badge="new"
-                    />
-                    <Card image={ProductOne} title="HAVIT HV-G92 Gamepad" saleprice="120" regularprice="160" badge="new"
-                    />
-                    <Card image={ProductOne} title="HAVIT HV-G92 Gamepad" saleprice="120" regularprice="160" badge="new"
-                    />
-                    <Card image={ProductOne} title="HAVIT HV-G92 Gamepad" saleprice="120" regularprice="160" badge="new"
-                    />
-                    <Card image={ProductOne} title="HAVIT HV-G92 Gamepad" saleprice="120" regularprice="160" badge="new"
-                    />
+                    {
+                        ApiData.map((item,index)=>(
+                            index < show &&
+                            <Card image={item.image} title={item.name} saleprice={item.saleprice} regularprice={item.regularprice} badge={item.badge}
+                        />
+                        
+                            
+                        ))
+                    }
+                   
+                   
                 </Flex>
-                <div className='text-center pt-[60px]'>
+                {
+                    show<ApiData.length&& <div onClick={()=>setShow(ApiData.length)} className='text-center pt-[60px]'>
                     <Button text="View All Products"/>
                 </div>
+                }
               
 
             </Container>
--- src/layouts/Today.jsx
+++ src/layouts/Today.jsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "../components/Container";
 import SubHeading from "../components/SubHeading";
 import Heading from "../components/Heading";
 import Card from "../components/Card";
 import Flex from "../components/Flex";
 import Button from "../components/Button";
-import ProductOne from "../assets/product1.png";
+import Product1 from "../assets/product1.png";
+import Product2 from "../assets/product2.png";
+import Product3 from "../assets/product3.png";
+import Product4 from "../assets/product4.png";
+import Product5 from "../assets/product5.png";
+import Product6 from "../assets/product6.png";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import NextArrow from "../components/NextArrow";
 import PrevArrow from "../components/PrevArrow";
+import Apidata from "../data";
 
 const Today = () => {
+  let [show, setShow] = useState(0)
+
+  // console.log(Apidata);
+
+
+
+
+
+
   var settings = {
     arrows: true,
     infinite: true,
@@ -31,80 +46,90 @@ const Today = () => {
           <Slider {...settings}>
             <div>
               <Card
-                image={ProductOne}
-                title="HAVIT HV-G92 Gamepad"
-                saleprice="120"
-                regularprice="160"
+                image={Product1}
+                title="Product One"
+                saleprice="300"
+                regularprice="200"
                 badge="new"
               />
             </div>
             <div>
               <Card
-                image={ProductOne}
-                title="HAVIT HV-G92 Gamepad"
-                saleprice="120"
-                regularprice="160"
+                image={Product2}
+                title="Product One"
+                saleprice="300"
+                regularprice="200"
                 badge="new"
               />
             </div>
             <div>
               <Card
-                image={ProductOne}
-                title="HAVIT HV-G92 Gamepad"
-                saleprice="120"
-                regularprice="160"
+                image={Product3}
+                title="Product One"
+                saleprice="300"
+                regularprice="200"
                 badge="new"
               />
             </div>
             <div>
               <Card
-                image={ProductOne}
-                title="HAVIT HV-G92 Gamepad"
-                saleprice="120"
-                regularprice="160"
+                image={Product4}
+                title="Product One"
+                saleprice="300"
+                regularprice="200"
                 badge="new"
               />
             </div>
             <div>
               <Card
-                image={ProductOne}
-                title="HAVIT HV-G92 Gamepad"
-                saleprice="120"
-                regularprice="160"
+                image={Product5}
+                title="Product One"
+                saleprice="300"
+                regularprice="200"
                 badge="new"
               />
             </div>
             <div>
               <Card
-                image={ProductOne}
-                title="HAVIT HV-G92 Gamepad"
-                saleprice="120"
-                regularprice="160"
+                image={Product6}
+                title="Product One"
+                saleprice="300"
+                regularprice="200"
                 badge="new"
               />
             </div>
-            <div>
-              <Card
-                image={ProductOne}
-                title="HAVIT HV-G92 Gamepad"
-                saleprice="120"
-                regularprice="160"
-                badge="new"
-              />
-            </div>
-            <div>
-              <Card
-                image={ProductOne}
-                title="HAVIT HV-G92 Gamepad"
-                saleprice="120"
-                regularprice="160"
-                badge="new"
-              />
-            </div>
+
           </Slider>
-          <div className="text-center mt-[60px]">
-            <Button text="View All Products" />
+
+
+
+
+
+
+          <div className="flex flex-wrap gap-4">
+            {
+              Apidata.slice(0, show).map(item => (
+                <div>
+                  <Card
+                    image={item.image}
+                    title={item.name}
+                    saleprice={item.saleprice}
+                    regularprice={item.regularprice}
+                    badge={item.badge}
+                  />
+                </div>
+
+              ))
+
+            }
+
           </div>
+          {
+            show < Apidata.length ? <div onClick={() => setShow(show + 4)} className="text-center mt-[60px]">
+              <Button text="Load More Product" />
+            </div>
+              : <p className="text-center text-3xl text-red-500">No More Product</p>
+          }
         </div>
       </Container>
     </section>
